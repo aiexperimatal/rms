5b1c578e7e895d933389f036be03018e27affa54
John B <warpedcodemonkey@yahoo.com>
2023-07-21 19:26:09 -0400

    Setup commit

---
+++ src/main/java/com/krazykritterranch/apps/RMS.java
@@ -0,0 +1,12 @@
+package com.krazykritterranch.apps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RMS {
+    public static void main(String[] args) {
+        SpringApplication.run(RMS.class, args);
+    }
+
+}
